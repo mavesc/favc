fc2990c786d070de0587b7b5b7b9531457a32f36
Mauricio Vargas <vargasmauricio65@gmail.com>
2025-12-11 16:16:26 -0400

    feat: exported modules for dev usage

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { VideoClipper } from './core/clipper';
+export { VideoAnalyzer } from './core/analyzer';
+export { TimecodeParser } from './core/timecode';
+export { ExtractionStrategies } from './core/strategies';
+export { extractCommand as clip } from './commands/extract';
+export { thumbnailCommand as getThumbnail } from './commands/thumbnail';
+export * from './types';
